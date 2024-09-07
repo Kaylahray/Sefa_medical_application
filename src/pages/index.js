5142537a2bc5277d905b31e4bf952cafa5644771
chioma <chiomachristopher91@gmail.com>
2024-09-07 18:53:23 +0100

    implemented routing. created pages as oultets. created navbar- starter code.

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import OverviewPage from "./OverviewPage";
+import Patient from "./Patient";
+import Staff from "./Staff";
+import Finance from "./Finance";
+import Pharmacy from "./Pharmacy";
+import Analytics from "./Analytics";
+import Labs from "./Labs";
+import Users from "./Users";
+export {
+  OverviewPage,
+  Patient,
+  Staff,
+  Finance,
+  Pharmacy,
+  Analytics,
+  Labs,
+  Users,
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,24 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        'AvenirMeduim' : ['Avenir-Meduim', 'Arial', 'sans-serif'],
+      fontFamily: {
+        AvenirMeduim: ["Avenir-Meduim", "Arial", "sans-serif"],
       },
       colors: {
-        bgColor: '#FAFAFA',
-        lightColorText: '#7A7A7A',
-        boldColorText: '#292929',
-        buttonBg: '#799794',
-        activeTextColor: '#03A300',
-        activerBgColor: '#EBFFEB',
-        inactiveTextColor: '#A35800',
-        inactiveBgColor: '#FFF6EB',
-        warning: '#CC0000',
-      }
+        bgColor: "#FAFAFA",
+        lightColorText: "#7A7A7A",
+        boldColorText: "#292929",
+        buttonBg: "#799794",
+        activeTextColor: "#03A300",
+        activerBgColor: "#EBFFEB",
+        inactiveTextColor: "#A35800",
+        inactiveBgColor: "#FFF6EB",
+        warning: "#CC0000",
+      },
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [],
+  },
 };
