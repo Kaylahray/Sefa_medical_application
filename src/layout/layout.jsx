9763f72b993dcd51bb7c9a1b4ff3870fe5fc4d3c
portable <emmanueltemitopedorcas20@gmail.com>
2024-09-08 22:11:15 +0100

    general font fixed

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Content/Navbar";
 const Layout = () => {
   return (
-    <div className="bg-bgColor h-screen">
+    <div className="bg-bgColor h-screen font-AvenirMeduim">
       <Navbar />
-      <div className="content conatiner px-4 md:px-0 md:w-4/5 max-w-7xl mx-auto py-4 font-AvenirMeduim">
+      <div className="content conatiner px-4 md:px-0 md:w-4/5 max-w-7xl mx-auto py-4 ">
         <Outlet /> {/* This is where child routes will be rendered */}
       </div>
     </div>
