07442caf78dc6ef22796348cfd06c699d07ba1d8
kamaldeen <kamalaliyu503@gmail.com>
2024-09-10 08:37:23 +0100

    feat(ui): add staff page

--- src/pages/staff/Staff.jsx
+++ src/pages/staff/Staff.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import InputLabel from '../../Components/shared/InputLabel'
+import StaffHeader from '../../Components/Content/StaffHeader'
+import StaffCards from '../../Components/Content/StaffCards'
+import StaffTable from '../../Components/Content/StaffTable'
 
 const Staff = () => {
   return (
     <div>
       Staff
+      <StaffHeader />
+      <StaffCards/>
+      <StaffTable/>
+      
     </div>
   )
 }
