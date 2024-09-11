31fc1a900ad7906b2559c963abf8b55057914f13
kamaldeen <kamalaliyu503@gmail.com>
2024-09-11 14:33:03 +0100

    staff fixed

--- src/Components/Content/staff/StaffCards.jsx
+++ src/Components/Content/staff/StaffCards.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import DashboardCard from '../../shared/DashboardCard'
+import Staff from "../../../assets/img/staff icon.svg";
 import Card from '../../shared/Card'
 import users from '../../../assets/img/users.svg'
 
@@ -6,12 +8,119 @@ import users from '../../../assets/img/users.svg'
 function StaffCards() {
   return (
     <div>
-      <Card>
-        
-      <img className='w-7 h-7' src={users} alt="users" />
-      <p>Total staff</p>
-      <h1>10,000</h1>
-      </Card>
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              {/* <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span> */}
+            </div>
+          </div>
+      </DashboardCard>
+      
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              {/* <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span> */}
+            </div>
+          </div>
+      </DashboardCard>
+
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              {/* <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span> */}
+            </div>
+          </div>
+      </DashboardCard>
+
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              {/* <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span> */}
+            </div>
+          </div>
+      </DashboardCard>
+
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span>
+            </div>
+          </div>
+      </DashboardCard>
+
+      <DashboardCard>
+      <div className="flex items-center gap-4 ">
+            {/* img div */}
+            <div className="img">
+              <img src={Staff} alt="patient icon" />
+            </div>
+            {/* side info inside card */}
+            <div className="content">
+              <span className="text-dasboardSmalText text-sm font-semibold">
+                New Patients
+              </span>
+              <p className="text-boldColorText text-3xl font-normal ">10,000</p>
+              <span className="text-activeTextColor text-sm font-semibold  ">
+                <FaArrowUp className="inline text-xs " /> 15% today
+              </span>
+            </div>
+          </div>
+      </DashboardCard>
     </div>
   )
 }
--- src/Components/Content/staff/StaffTable.jsx
+++ src/Components/Content/staff/StaffTable.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import Table from '../../shared/Table'
 import TableHeader from '../../shared/TableHeader'
 import TableBody from '../../shared/TableBody'
 import { useContext } from 'react'
+import UsersContext from "../../../context/AuthContext";
+import { IoMdMore } from "react-icons/io";
+import { Link } from 'react'
 
 function StaffTable() {
+  const { people, loading } = useContext(UsersContext);
+  const users = people.slice(0, 10);
+  const handleClick = () => {
+    const menu = document.getElementById('moreMenu')
+    if(menu.style.display === 'none'){
+        menu.style.display = 'block'
+    } else{
+        menu.style.display = 'none'
+    }
+}
   return (
     <div>
       <TableHeader>
@@ -12,13 +24,13 @@ function StaffTable() {
                 scope="col"
                 className="min-w-[] pl-3 "
               >
-                Name
+                I.D
               </th>
               <th
                 scope="col"
                 className="px-3"
               >
-                Title
+                Name
               </th>
               <th scope="col" className="px-3">
                 Email Address
@@ -37,78 +49,55 @@ function StaffTable() {
               </th>
       </TableHeader>
       <TableBody>
-
-      import React from 'react';
-
-const UserList = ({ users }) => {
-  return (
-    <ul>
-      {users.map(user => (
-        <li key={user.id}>
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-
-
-
-
-
-
-      
-
+      {users.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <td className="relative px-3 py-7 sm:w-12 sm:px-6 border-r-0 rounded-s-lg border border-[#E0E0E0]">
+                    <input
+                      type="checkbox"
+                      className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-[#8F8F8F] text-indigo-600 focus:ring-indigo-600"
+                    />
+                  </td>
+                  <td className="px-3 border border-[#E0E0E0] border-x-0 ">
+                    {user.id}
+                  </td>
+                  <td className="px-3 border border-[#E0E0E0] border-x-0 ">
+                    {user.firstName} {user.lastName}
+                  </td>
+                  <td className="px-3 border border-[#E0E0E0] border-x-0 normal-case ">
+                    {user.email}
+                  </td>
+                  <td className="px-3 border border-[#E0E0E0] border-x-0 ">
+                    Doctor
+                  </td>
+                  <td className="px-3 border border-[#E0E0E0] border-x-0 ">
+                    <span className='bg-activerBgColor text-activeTextColor px-5 py-3 text-center'>Active</span>
+                  </td>
+                  <td className="px-3 rounded-e-lg border border-[#E0E0E0] border-l-0">
+                  <IoMdMore onClick={() =>{handleClick()}} className="cursor-pointer" />
+                  <div id="moreMenu" className="hidden absolute right-2 md:right-28 z-10 mt-2 w-28 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none">
+                    <div className="py-1">
+                      <Link
+                        to={'/staff'}
+                        className="block px-4 py-2 text-sm border-b hover:bg-[#E4EAEA] "
+                      >
+                        View
+                      </Link>
+                      <Link
+                        to={'/labs'}
+                        className="block px-4 py-2 text-sm hover:bg-[#E4EAEA]"
+                      >
+                        Edit
+                      </Link>
+                    </div>
+                  </div>
+                  </td>
+                </tr>
+              );
+            })}
     {/* map here */}
       </TableBody>
-      <Table className='pt-7' >
-        <table className=" mt-20 w-full border-separate border-spacing-y-2 text-tableTextColor">
-          <thead className="bg-[#E4EAEA] text-left text-base  font-semibold">
-            <tr>
-              <th
-                scope="col"
-                className="relative px-7 py-6 sm:w-12 sm:px-6 rounded-tr-none rounded-t-lg"
-              >
-                <input
-                  type="checkbox"
-                  className="absolute left-4 top-1/2 -mt-2 h-4 w-4  rounded border-[#8F8F8F] text-indigo-600 focus:ring-indigo-600"
-                />
-              </th>
-              
-            </tr>
-          </thead>
 
-        {/* cut this one and put up in body  */}
-          <tbody className="bg-white font-normal text-lg">
-            <tr>
-              <td className="relative px-3 py-7 sm:w-12 sm:px-6 border-r-0 rounded-s-lg border border-[#E0E0E0]">
-                <input
-                  type="checkbox"
-                  className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-[#8F8F8F] text-indigo-600 focus:ring-indigo-600"
-                />
-              </td>
-              <td className="px-3 border border-[#E0E0E0] border-x-0 ">
-                al
-              </td>
-              <td className="px-3 border border-[#E0E0E0] border-x-0 ">
-                alkjhgfdsawertyuiopiklm
-              </td>
-              <td className="px-3 border border-[#E0E0E0] border-x-0 ">
-                alkjhgfdsawertyuiopiklm
-              </td>
-              <td className="px-3 border border-[#E0E0E0] border-x-0 ">
-                alkjhgfdsawertyuiopiklm
-              </td>
-              <td className="px-3 rounded-e-lg border border-[#E0E0E0] border-l-0">
-               
-              </td>
-            </tr>
-          </tbody>
-        </table>
-    
-      </Table>
     </div>
   )
 }
