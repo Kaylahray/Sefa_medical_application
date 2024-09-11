5fb368a33a0aed46110b4aa27e7439c397e4d3a2
Abraham <abrahamojobo7@gmail.com>
2024-09-11 14:33:39 +0100

    pharmacy page updated. added cards and buttons

--- src/pages/pharmacy/Pharmacy.jsx
+++ src/pages/pharmacy/Pharmacy.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react'
+import PharmacyMdeication from '../../Components/Content/pharmacy/PharmacyMdeication'
 
 const Pharmacy = () => {
-  return <div>Pharmacyy</div>;
-};
+  return (
+    <div>
+      <PharmacyMdeication />
+    </div>
+  )
+}
 
-export default Pharmacy;
+export default Pharmacy
