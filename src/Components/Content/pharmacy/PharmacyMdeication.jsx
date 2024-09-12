8a26f0266bee78120fcffce17fd43168c249ad4a
Abraham <abrahamojobo7@gmail.com>
2024-09-12 15:41:10 +0100

    updated pharmacy page

--- src/Components/Content/pharmacy/PharmacyMdeication.jsx
+++ src/Components/Content/pharmacy/PharmacyMdeication.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import DashboardCard from '../../shared/DashboardCard';
 import box1 from "../../../assets/pbox1.svg"
+import box2 from "../../../assets/pbox2.svg"
 import Search from '../../shared/Search';
 import Select from '../../shared/Select';
 import Button from '../../shared/Button';
+
+
 const Statistics = () => {
   return (
 
     <div className="grid grid-cols-1 md:grid-cols-2 items-center">
-    <h1>Patients Report</h1>
+    <h1 className="text-xl font-semibold mb-4">Pharmacy</h1>
     <div className="flex flex-row gap-3 items-center justify-end ">
       <Search />
       <Select />
-<Button> New Medication</Button>
+<Button class="p-17px, 106px, 17px, 106px"> New Medication</Button>
     </div>
 
     <div className="flex flex-col md:flex-row gap-4 mb-6">
@@ -36,7 +39,7 @@ const Statistics = () => {
           <div className="flex items-center gap-4  ">
             {/* img div */}
             <div className="img">
-              <img src={box1} alt="staff icon" />
+              <img src={box2} alt="staff icon" />
             </div>
             {/* side info inside card */}
             <div className="content">
@@ -48,19 +51,12 @@ const Statistics = () => {
           </div>
         </DashboardCard>
 
-
-
-    
     </div>
 
-
-
   </div>
-
-
-
     
   );
 }
 
 export default Statistics;
+
