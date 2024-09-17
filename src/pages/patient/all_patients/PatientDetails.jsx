66a4338c9755a441f1cb1e5e26c41d9b611d99f2
chioma <chiomachristopher91@gmail.com>
2024-09-17 07:26:26 +0100

    updated patients all page

--- src/pages/patient/all_patients/PatientDetails.jsx
+++ src/pages/patient/all_patients/PatientDetails.jsx
@@ -1,12 +1,12 @@
 import TabComponent from "../../../Components/Content/analytics/TabComponent";
 import BlankTab from "../../analytics/BlankTab";
 import back from "../../../assets/back.svg";
-import PatientCard from "../../../Components/Content/patient/PatientCard";
 import BiodataDetails from "../../../Components/Content/patient/BiodataDetails";
 import MedicalAccordion from "../../../Components/Content/patient/MedicalAccordion";
 import HMOInfoCard from "../../../Components/Content/patient/HMOInfoCard";
 import ConsultationAccordion from "../../../Components/Content/patient/ConsultationAccordion";
 import FinanceCard from "../../../Components/Content/patient/FinanceCard";
+import { Link } from "react-router-dom";
 const Biodata = () => <BlankTab content={<BiodataDetails />} />;
 
 const Hmo = () => <BlankTab content={<HMOInfoCard />} />;
@@ -23,9 +23,9 @@ const PatientDetails = () => {
     <>
       <div className="w-full">
         <div className=" px-8 py-2">
-          <div>
+          <Link to="/patient/all">
             <img src={back} alt="" srcset="" />
-          </div>
+          </Link>
         </div>
 
         <TabComponent tabs={tabData} />
