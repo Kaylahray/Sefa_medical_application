4f5763edc801395da6b2b9345cfb04a444f73f0b
PeterOche <petergoddey08@gmail.com>
2024-09-18 20:25:25 +0100

    new commit

--- src/Components/Content/staff/StaffHeader.jsx
+++ src/Components/Content/staff/StaffHeader.jsx
@@ -1,28 +1,22 @@
-import React from 'react'
-import Search from '../../shared/Search'
-import Select from '../../shared/Select'
-import Button from '../../shared/Button'
-import { Link } from 'react-router-dom'
-
-
-
-
+import React from "react";
+import Search from "../../shared/Search";
+import Select from "../../shared/Select";
+import Button from "../../shared/Button";
+import { Link } from "react-router-dom";
 
 function StaffHeader() {
   return (
-    <div className=' flex gap-16'>
-    
-  
-        <Search />
-        <Select/>
-      
-        <Button><Link to={'/staff/newstaff'}>New Staff</Link></Button>
-    
-  
-      
-    
+    <div className=" flex gap-16">
+      <Search />
+      <div>
+        <Select />
+      </div>
+
+      <Button>
+        <Link to={"/staff/newstaff"}>New Staff</Link>
+      </Button>
     </div>
-  )
+  );
 }
 
-export default StaffHeader
+export default StaffHeader;
